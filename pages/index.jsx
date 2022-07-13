4373c50e6de9e9e9e088c9d2931fdd4d84219a58
Brent Lombaard <99014529+Brento27@users.noreply.github.com>
2022-07-14 00:18:22 +0200

    Finished Blog app

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head"
 import { getPosts } from "../services"
-
+import { FeaturedPosts } from "../sections"
 import { PostCard, Categories, PostWidget } from "../components"
 
 const Home = ({ posts }) => {
@@ -10,6 +10,7 @@ const Home = ({ posts }) => {
         <title>Brent Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
