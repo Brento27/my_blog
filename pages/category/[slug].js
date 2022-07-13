a0c46ea1dcdaacf35eb6b13355f780b0d3b7fb49
Brent Lombaard <99014529+Brento27@users.noreply.github.com>
2022-07-14 00:51:29 +0200

    Fixed static pages not loading

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,14 +1,14 @@
-import React from "react"
-import { useRouter } from "next/router"
+import React from 'react';
+import { useRouter } from 'next/router';
 
-import { getCategories, getCategoryPost } from "../../services"
-import { PostCard, Categories, Loader } from "../../components"
+import { getCategories, getCategoryPost } from '../../services';
+import { PostCard, Categories, Loader } from '../../components';
 
 const CategoryPost = ({ posts }) => {
-  const router = useRouter()
+  const router = useRouter();
 
   if (router.isFallback) {
-    return <Loader />
+    return <Loader />;
   }
 
   return (
@@ -26,25 +26,25 @@ const CategoryPost = ({ posts }) => {
         </div>
       </div>
     </div>
-  )
-}
-export default CategoryPost
+  );
+};
+export default CategoryPost;
 
 // Fetch data at build time
 export async function getStaticProps({ params }) {
-  const posts = await getCategoryPost(params.slug)
+  const posts = await getCategoryPost(params.slug);
 
   return {
     props: { posts },
-  }
+  };
 }
 
 // Specify dynamic routes to pre-render pages based on data.
 // The HTML is generated at build time and will be reused on each request.
 export async function getStaticPaths() {
-  const categories = await getCategories()
+  const categories = await getCategories();
   return {
     paths: categories.map(({ slug }) => ({ params: { slug } })),
     fallback: true,
-  }
+  };
 }
